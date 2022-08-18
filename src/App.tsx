87dd9efdc1197b35f462d0194f5304be14482f07
Joel <joel.r.maxwell@gmail.com>
2022-08-18 19:36:06 +1000

    Added basic routing and Redux Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 function App() {
   return (
     <>
-      <p className='text-lg font-bold text-red-600'>App Page</p>
+      <Router>
+        <div className = "w-[100vw] h-[100vh] bg-red-400">
+          <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
