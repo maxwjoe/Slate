266a52cfec293218e7e74bbb4ea152539a107d79
Joel <joel.r.maxwell@gmail.com>
2022-08-20 18:27:10 +1000

    Locked dashboard to signed in users only

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../redux/hooks';
 import ArticleView from '../components/ArticleView';
 import Header from '../components/Header';
 import LeftTree from '../components/LeftTree';
 import ListView from '../components/ListView';
 
 function Dashboard() {
+
+  const navigate = useNavigate();
+  const {user} = useAppSelector((state) => state.auth);
+
+  // useEffect to lock the dashboard to authorized users
+  useEffect(() => {
+    if(!user)
+    {
+      navigate('/login')
+    }
+  }, [user, navigate])
+
   return (
     <div className='flex flex-col w-full h-full bg-slate-dark'>
       <Header/>
